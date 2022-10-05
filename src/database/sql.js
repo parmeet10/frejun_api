d1c6348a48c94f1f4d50c1f71333060534285f84
parmeet <sparmeet162000@gmail.com>
2022-10-06 00:51:11 +0530

    server and database setup

---
+++ src/database/sql.js
@@ -0,0 +1,24 @@
+const mysql = require("mysql2/promise");
+
+const wrapperService = require("../services/wrapper");
+
+const Knex = require("knex");
+
+const mysqlConnectionString = {
+  host: "127.0.0.1",
+  user: "frejunUser",
+  password: "frejunUser",
+  database: "frejunTestDatabase",
+  port: 3306,
+};
+
+const KNEX_CONFIG = {
+  client: "mysql",
+  connection: mysqlConnectionString,
+};
+
+const knex = Knex(KNEX_CONFIG);
+const data = {
+  knex: knex,
+};
+module.exports = data;
